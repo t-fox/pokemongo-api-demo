a36c35d893fa1aac9758f741cdc580de85e6e18a
memelyfe <lilrnohngwl6xq6u2k@thrott.com>
2016-07-18 12:50:15 -0400

    better error messages

--- main.py
+++ main.py
@@ -145,12 +145,14 @@ def api_req(api_endpoint, access_token, *mehs, **kw):
             p_ret = pokemon_pb2.ResponseEnvelop()
             p_ret.ParseFromString(r.content)
 
+            '''
             if DEBUG:
                 print("REQUEST:")
                 print(p_req)
                 print("Response:")
                 print(p_ret)
                 print("\n\n")
+            '''
 
             if DEBUG:
                 print("[ ] Sleeping for 1 second")
@@ -194,11 +196,15 @@ def get_profile(access_token, api, useauth, *reqq):
     return api_req(api, access_token, req, useauth = useauth)
 
 def get_api_endpoint(access_token, api = API_URL):
-    p_ret = get_profile(access_token, api, None)
-    try:
-        return ('https://%s/rpc' % p_ret.api_url)
-    except:
-        return None
+    while True:
+        try:
+            p_ret = get_profile(access_token, api, None)
+            if ".com" in p_ret:
+                return ('https://%s/rpc' % p_ret.api_url)
+        except:
+            pass
+        print('[-] Error getting API endpoint (server may be down), retrying')
+        time.sleep(2)
 
 
 def login_ptc(username, password):
